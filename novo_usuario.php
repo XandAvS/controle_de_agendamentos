60d0ae2edd891d1b7e62f5c87f0dbb4585bcffaa
Alexandre Alves <alexandre.alks.alves@gmail.com>
2025-05-05 00:16:29 -0300

    Inicio de cadastro de novos usuários

---
+++ novo_usuario.php
@@ -0,0 +1,23 @@
+<?php 
+    //sempre que for começar uma função de virificar no banco
+    //devoemos conectar antes
+    require('conexao.php');
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        try{
+
+        }catch (Exception $e){
+
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Novo Usuário</title>
+</head>
+<body>
+    
+</body>
+</html>
