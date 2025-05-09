3ebbbce85dcd75e3951ed9040dae5ce2d5c5b5aa
Alexandre Alves <alexandre.alks.alves@gmail.com>
2025-05-08 23:26:28 -0300

    Inicio de novo usuário

--- novo_usuario.php
+++ novo_usuario.php
@@ -4,9 +4,24 @@
     require('conexao.php');
     if($_SERVER['REQUEST_METHOD'] == "POST"){
         try{
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $senha = password_hash(password: $_POST['senha'], algo: PASSWORD_BCRYPT);
+             //criptografar a senha com a função password_hash
+            // PASSWORD_BCRYPT é o algoritimo para a criptografia
 
+              // inserção no banco de dados
+            $stmt = $pdo->prepare("INSERT INTO usuarios(nome, email, senha)
+                    VALUES (?,?,?)");
+            if($stmt->execute([$nome, $email, $senha])){
+                // DECLARAR OS PARAMETROS COMO ? PARA PREPARAR O BANCO
+                header("location: index.php?cadastro=true");                                               // NA ESTRUTURA DA TABELA
+            }              // isso que incrementa no banco de dados 
+            else{
+                echo "<p> Erro ao inserir o usuario</p>";
+            }               
         }catch (Exception $e){
-
+            echo "erro ao inserir usuario". $e->getMessage();
         }
     }
 ?>
@@ -18,6 +33,10 @@
     <title>Novo Usuário</title>
 </head>
 <body>
-    
+    <form action="post">
+        <div >
+
+        </div>
+    </form>    
 </body>
 </html>
