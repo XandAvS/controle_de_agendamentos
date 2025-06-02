0411942c247871e38e1c54d2414eaee305e0ed49
Alexandre Alves <alexandre.alks.alves@gmail.com>
2025-06-02 00:47:36 -0300

    Update home.php

--- home.php
+++ home.php
@@ -27,23 +27,7 @@ function retornaProfissionais()
 
 $profissionais = retornaProfissionais();
 ?>
-<!DOCTYPE html>
-<html lang="pt-BR">
 
-<head>
-    <meta charset="UTF-8" />
-    <title>Serviços com Snap</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="./src/css/estilos.css">
-    <link rel="stylesheet" href="./src/css/cartao.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-
-</head>
-
-<body>
 
     <div class="container">
         <h4 class="mb-3 nomenclaturas">SELECIONE OS SERVIÇOS:</h4>
